42d520747090e732b969ba50360120328edfed70
keiko020509 <keikomelody0205009@gmail.com>
2024-10-11 15:43:26 +0700

    change title.tsx language

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import '../styles/Title.css';
 export default function Title() {
     return (
         <div className="title">
-            <h1><span className="first-part">Colour </span>
+            <h1><span className="first-part">Warna </span>
              <span className="second-part">Memo</span></h1>
         </div>
     )
